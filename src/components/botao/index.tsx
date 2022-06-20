0542228b59f9dcbf22f59f3eedf533bc672102ae
ThiagoPertile <thiagomcarmo@outlook.com>
2022-06-20 17:45:50 -0300

    Fiz componente: FORMULARIO

---
+++ src/components/botao/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Botao extends React.Component {
+    render() {
+        return (
+         <button>
+             Click Me
+         </button>   
+        )
+    }
+}
+
+export default Botao;
