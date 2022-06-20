b545c984920b1e74ebcc66168421b5b3d59b3d2b
ThiagoPertile <thiagomcarmo@outlook.com>
2022-06-20 20:26:28 -0300

    Fiz componente: LISTA

---
+++ src/components/lista/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Lista() {
+    return (
+      <aside>
+         <ul>
+            <li>
+               <h3>
+                  React
+               </h3>
+               <span>
+                  02:00:00
+               </span>
+            </li>
+            <li>
+               <h3>
+                  Java Script
+               </h3>
+               <span>
+                  01:00:00
+               </span>
+            </li>
+         </ul>
+      </aside>
+    )
+}
+
+export default Lista;
